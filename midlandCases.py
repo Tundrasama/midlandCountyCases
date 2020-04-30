f007bcdb1b8043e8bdba8b842a7833bef9f455b5
Jeremy McGraw <tundrasama@gmail.com>
2020-04-29 23:04:53 -0500

    updating csvs

--- midlandCases.py
+++ midlandCases.py
@@ -1,5 +1,7 @@
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def PD_main():
 	url = 'http://www.midlandtexas.gov/232/Health-Senior-Services'
@@ -15,6 +17,22 @@ def PD_main():
 		if i == 1:
 			print("Source of Exposure")
 			dfName="Source_of_Exposure.csv"
+			# print(df[df.columns[0:2]])
+
+			# x_axis = df[df.columns[0]]
+			# y_axis = df[df.columns[1]]
+		
+			# y_pos = np.arange(len(x_axis))
+			# performance = [10,8,6,4]
+
+			# plt.bar(y_pos, y_axis, align='center', alpha=0.5)
+			# plt.xticks(y_pos, x_axis,rotation=45)
+			# plt.ylabel('Cases')
+			# plt.tight_layout()
+			# plt.title(dfName.replace('_',' ').replace('.csv',''),fontsize=20)
+
+			# plt.show()
+			
 		if i == 2:
 			print("Gender")
 			dfName="Gender.csv"
@@ -31,7 +49,8 @@ def PD_main():
 			print("Investigation Status")
 			dfName="Investigation_Status.csv"
 	
-		df.to_csv(dfName,index=False)
+		# df.to_csv(dfName,index=False)
 
 if __name__ == "__main__":
+	plt.rcParams['toolbar'] = 'None'
 	PD_main()
